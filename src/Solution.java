76a1209f87704dfe9fb0d0f0ede931f297d74c35
user <danilovsansan@mail.ru>
2023-12-11 15:46:00 +0300

    Дополниельные задачи по курсу 1 сохранение 1 задачи

--- src/Solution.java
+++ src/Solution.java
@@ -1,55 +1,10 @@
 
     public class Solution {
             public static void main(String[] args) {
-                // цикл while
-                //String solution =  "Хочешь - решай задачи, не хочешь - решай нехотя";
-                /*int a = 16;
-                while (a >= 0) {
-                    System.out.println(solution);
-                    a --;
-                }*/
-               /* int count = 1;
-                while (true) {
-                    System.out.println("Строка №" + count);
-                    System.out.println(solution);
-                    if (count > 16) {
-                        break;
-                    }
-                    count = count + 1; // Без наращивания цикл будет выполняться вечно
-                }
-               */
-              //  Цикл do..while
-               /* int a = 1;
-                do {
-                    System.out.println("Строка №" + a);
-                    System.out.println(solution);
-                    a ++;
-                } while (a <= 16); */
-
-              //  Цикл for
-
-                   /* for (int a = 1; a <= 25; a++) {
-                        System.out.println("Строка №" + a );
-                        System.out.println(solution);
-                    */
-
-               /* String[] daysOfWeek =
-                        { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
-
-
-                for (String dayOfWeek : daysOfWeek) {
-                   System.out.println(dayOfWeek);
-                }
-               */
-
-
            /* 1.2 Переменные
-
             1 задание
-
                У вас есть две переменных, в которых хранятся два целочисленных числа. Напишите программу,
                которая выводит в консоль результат деления первого числа на второе с остатком.
-
                В качестве исходных данных используйте значения для первого числа *– 47, для второго – 13):*
                В результате деления 47 / 13
                Выходит 3 и 8 в остатке
