719701b9145ff1fde8ae20261c8e5830066d6535
user <danilovsansan@mail.ru>
2023-12-11 15:21:28 +0300

    Дополниельные задачи по курсу 1 сохранение 1 задачи

--- src/Solution.java
+++ src/Solution.java
@@ -33,13 +33,33 @@
                         System.out.println(solution);
                     */
 
-                String[] daysOfWeek =
+               /* String[] daysOfWeek =
                         { "Понедельник", "Вторник", "Среда", "Четверг", "Пятница", "Суббота", "Воскресенье" };
 
 
                 for (String dayOfWeek : daysOfWeek) {
-                    System.out.println(dayOfWeek);
+                   System.out.println(dayOfWeek);
                 }
+               */
+
+
+           /* 1.2 Переменные
+
+            1 задание
+
+               У вас есть две переменных, в которых хранятся два целочисленных числа. Напишите программу,
+               которая выводит в консоль результат деления первого числа на второе с остатком.
+               В качестве исходных данных используйте значения для первого числа *– 47, для второго – 13):*
+               В результате деления 47 / 13
+               Выходит 3 и 8 в остатке
+             */
+             int integer1 = 47;
+             int integer2 = 13;
+             //int remainder = 47%13;
+             float remainder ( 47/100 )*13f;
+             double result = integer1 / integer2;
+             System.out.println("В результате деления двух переменных , integer1 на integer2, выходит "+ result+ " и "+ remainder+ " в остатке ." );
+
 
                 }
             }
